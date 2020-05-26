4daa13e53943435273a6a6154962e54b3c53eb67
kangert <kangert@qq.com>
2020-05-26 20:59:01 +0800

    删除无用代码

--- src/components/admin/studentData.vue
+++ src/components/admin/studentData.vue
@@ -1195,19 +1195,7 @@ export default {
       /** 回访信息表单数据 */
       returnVisitForm: {
         studentCode: '',
-        studentName: '',
-        data1: '',
-        data2: '',
-        data3: '',
-        data4: '',
-        data5: '',
-        data6: '',
-        data7: '',
-        data8: '',
-        data9: '',
-        data10: '',
-        data11: '',
-        data12: ''
+        studentName: ''
       }
     }
   },
